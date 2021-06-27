c279c7d65c6b7791241b7a1c9c35998a7f34b2c7
MRALADAG <63924287+MRALADAG@users.noreply.github.com>
2021-06-27 08:45:06 +0300

    26.06.2021 Northwind frontend

--- src/store/initialValues/cartItems.js
+++ src/store/initialValues/cartItems.js
@@ -1,1 +1,3 @@
 export const cartItems = []
+// export const cartItems = [{ quantity: 11, product: { productName: "Eti Burçak Fıstık Ezmeli" } }]
+// Örneğin biz burada başlangıç default parametre tanımlayacak olsaydık yukarıdaki gibi tanımlayacaktık.
